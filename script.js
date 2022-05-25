45c3f7d2b6be7ce8d41a3c3d9b4c4c9cddb506ab
carlo catayna <carlocatayna13@gmail.com>
2022-05-25 13:30:34 +0800

    fixed dynamic adding of square boxes

---
+++ script.js
@@ -0,0 +1,45 @@
+
+let MAX_WIDTH = document.getElementById("sketchpad").offsetWidth;
+let MAX_HEIGHT = document.getElementById("sketchpad").offsetHeight;
+
+console.log(MAX_HEIGHT)
+console.log(MAX_WIDTH)
+console.log("Hello")
+
+const sketchpad_container = document.querySelector("#sketchpad");
+
+console.log(sketchpad_container)
+
+function applyChanges(){
+    let color = document.getElementById("inputcolor").value;
+    let dimension = document.getElementById("inputrange").value;
+
+    console.log(color);
+    console.log(dimension);
+
+    let square_dimension = MAX_HEIGHT/dimension;
+    let converteddimension = String(square_dimension) + "px";
+
+    for(let i=0;i<dimension*dimension;i++){
+        new_div = document.createElement('div');
+        new_div.style.cssText = 'box-sizing:border-box; display:flex; color: orange; background-color: pink; border: 1px solid; border-style:outset';          
+        new_div.style.width = converteddimension;
+        new_div.style.height = converteddimension;
+        
+        console.log(new_div.style.width);
+        console.log(new_div.style.height);
+        //new_div.textContent = "w";
+        //new_div.classList.add('sketch-box');
+        //console.log(new_div.style.width);
+        sketchpad_container.appendChild(new_div);
+        //console.log("adding a new div!");
+        
+    }
+    
+}
+
+
+const apply_button = document.getElementById("changebutton");
+
+apply_button.addEventListener('click',applyChanges);
+
